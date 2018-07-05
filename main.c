bc8a82fc8c01ac65b365604003cc88551a244338
Cole <eaehesap@gmail.com>
2018-07-05 22:31:30 +0300

    C/C++ Temel Program Örnekleri

---
+++ main.c
@@ -0,0 +1,59 @@
+//
+//  main.c
+//  C_calismalari
+//
+//  Created by Sait Alay on 5.07.2018.
+//  Copyright © 2018 Sait Alay. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("Yeniden Merhaba C\n");
+    return 0;
+}
+*/
+/*
+// C programı basit faiz hesaplama Programı
+void main()
+{
+    float p, r, t, si;
+    printf("Ana para, Faiz oranı ve vade değerleri girin;");
+    scanf("%f%f%f", &p,&r, &t);
+    si = (p*r*t)/ 100;
+    printf("\nSabit Faiz Tutarı = %f \n", si);
+    getchar();
+}
+*/
+/*
+//C Programı: Bir sayının üssünü alan program
+
+void main()
+{
+    int x, n, sonuc;
+    printf("Üs alınacak sıyıyı giriniz: ");
+    scanf("%d%d", &x,&n);
+    sonuc = pow(x,n);
+    printf("\n İstenen sayının üssü :%d olarak bulunmuştur. \n", sonuc);
+    getchar();
+}
+
+//C Programı: Girilen gün sayını  Yıl, Hafta ve Gün olarak hesaplayan program
+int main()
+{
+    int y, h, g, a;
+    printf("Bir sayı giriniz:");
+    scanf("%d", &g);
+    y = g / 365;
+    a = g % 365;
+    h = a / 7;
+    g = a % 7;
+    printf("\nYıl = %d\nHafta = %d\nGün = %d\n", y,h,g);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+
